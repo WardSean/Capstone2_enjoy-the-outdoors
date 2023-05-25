b5b77bf6e25e63253edcff61ebd5354a8b6539d1
Sean T. Ward <130578074+WardSean@users.noreply.github.com>
2023-05-25 19:26:29 -0400

    Add files via upload

--- Capstone2/scripts/mountains_information.js
+++ Capstone2/scripts/mountains_information.js
@@ -3,6 +3,13 @@ var mountainSelect = document.getElementById("mountainSelect");
 
 // Function to populate the dropdown menu with mountain options
 function populateMountainDropdown() {
+  // Sort the mountainsArray alphabetically by name (ignoring "Mt. ")
+  mountainsArray.sort(function (a, b) {
+    var nameA = a.name.replace(/^Mt\. /i, "").toLowerCase();
+    var nameB = b.name.replace(/^Mt\. /i, "").toLowerCase();
+    return nameA.localeCompare(nameB);
+  });
+
   // Add "All" option
   var allOption = document.createElement("option");
   allOption.value = "all";
@@ -17,34 +24,60 @@ function populateMountainDropdown() {
   }
 }
 
-async function getSunriseAndSunsetForMountain(lat, lng) {
-  try {
-    var response = await fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&formatted=0`);
-    var data = await response.json();
 
-    if (response.ok) {
-      var sunriseUTC = new Date(data.results.sunrise);
-      var sunsetUTC = new Date(data.results.sunset);
 
-      var options = {
-        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-        hour: '2-digit',
-        minute: '2-digit'
-      };
+// Function to save favorited cards as a CSV file
+function saveFavoritesAsCSV() {
+  // Filter the favorite cards
+  var favoriteCards = document.querySelectorAll(".card.favorite");
 
-      var sunriseTime = sunriseUTC.toLocaleTimeString([], options);
-      var sunsetTime = sunsetUTC.toLocaleTimeString([], options);
+  // Create a CSV string with the card details
+  var csvContent = "data:text/csv;charset=utf-8,";
+  csvContent += "Name,Description,Elevation\n"; // CSV header
 
-      return `${sunriseTime} to ${sunsetTime}`;
-    } else {
-      throw new Error("Failed to retrieve sunrise and sunset data");
-    }
-  } catch (error) {
-    console.error(error);
-    return "Error retrieving sunrise and sunset data";
+  for (var card of favoriteCards) {
+    var name = card.querySelector(".card-title").textContent;
+    var desc = card.querySelector(".card-text").textContent;
+    var elevation = card.querySelector(".card-text.card-elevation").textContent;
+
+    var row = [name, desc, elevation];
+    csvContent += row.join(",") + "\n";
   }
+
+  // Create a Blob object with the CSV data
+  var blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+
+  // Generate a temporary URL for the Blob object
+  var url = URL.createObjectURL(blob);
+
+  // Create a link element and set its attributes
+  var link = document.createElement("a");
+  link.setAttribute("href", url);
+  link.setAttribute("download", "favorites.csv");
+
+  // Simulate a click on the link to start the download
+  link.click();
 }
 
+// Function to display favorite cards
+function displayFavoriteCards() {
+  // Filter the favorite cards
+  var favoriteCards = document.querySelectorAll(".card.favorite");
+
+  // Clear existing cards
+  var mountainDetails = document.getElementById("mountainDetails");
+  mountainDetails.innerHTML = "";
+
+  // Create a row div to hold the favorite cards
+  var rowDiv = document.createElement("div");
+  rowDiv.classList.add("row");
+
+  for (var card of favoriteCards) {
+    rowDiv.appendChild(card);
+  }
+
+  mountainDetails.appendChild(rowDiv);
+}
 
 // Function to display mountain information based on the selected mountain
 function displayMountainDetails() {
@@ -54,7 +87,7 @@ function displayMountainDetails() {
   // Clear existing cards and sunrise/sunset information
   mountainDetails.innerHTML = "";
 
-  if (selectedMountain === "all") {
+  if (selectedMountain === "all" || selectedMountain === "") {
     // Show all mountains
     var mountainRows = chunkArray(mountainsArray, 3);
     for (var row of mountainRows) {
@@ -70,13 +103,25 @@ function displayMountainDetails() {
     }
   } else {
     // Find the selected mountain in the mountainsArray
-    var selectedMountainObj = mountainsArray.find(function(mountain) {
+    var selectedMountainObj = mountainsArray.find(function (mountain) {
       return mountain.name === selectedMountain;
     });
     if (selectedMountainObj) {
       var card = createMountainCard(selectedMountainObj);
       mountainDetails.appendChild(card);
     }
+  }
+
+  // Check if any favorites exist
+  var favoriteCards = document.querySelectorAll(".card.favorite");
+  if (favoriteCards.length === 0) {
+    // No favorites, hide the "Favorites" section
+    var favoritesSection = document.getElementById("favoritesSection");
+    favoritesSection.style.display = "none";
+  } else {
+    // Favorites exist, show the "Favorites" section
+    var favoritesSection = document.getElementById("favoritesSection");
+    favoritesSection.style.display = "block";
   }
 }
 
@@ -104,6 +149,22 @@ function createMountainCard(mountain) {
   var closeIcon = document.createElement("i");
   closeIcon.classList.add("fa", "fa-times", "card-close");
 
+  var starButtonContainer = document.createElement("div");
+  starButtonContainer.classList.add("star-button-container");
+
+  var starButton = document.createElement("button");
+  starButton.classList.add("star-button");
+
+  var starIcon = document.createElement("img");
+  starIcon.src = "images/star_unfav.png";
+  starIcon.alt = "Star";
+
+  starButton.appendChild(starIcon);
+  starButtonContainer.appendChild(starButton);
+  cardHeader.appendChild(starButtonContainer);
+  cardHeader.appendChild(lockIcon);
+  cardHeader.appendChild(closeIcon);
+
   var cardBody = document.createElement("div");
   cardBody.classList.add("card-body");
 
@@ -122,14 +183,13 @@ function createMountainCard(mountain) {
 
   var cardElevation = document.createElement("p");
   cardElevation.classList.add("card-text");
+  cardElevation.classList.add("card-elevation");
   cardElevation.textContent = "Elevation: " + mountain.elevation;
 
   var cardSunriseSunset = document.createElement("p");
   cardSunriseSunset.classList.add("card-text", "font-weight-bold");
   cardSunriseSunset.textContent = "Sunrise and Sunset: Calculating...";
 
-  cardHeader.appendChild(lockIcon);
-  cardHeader.appendChild(closeIcon);
   cardBody.appendChild(cardTitle);
   cardBody.appendChild(cardImg);
   cardBody.appendChild(cardDesc);
@@ -144,19 +204,170 @@ function createMountainCard(mountain) {
 
   // Get sunrise and sunset times for the mountain
   getSunriseAndSunsetForMountain(latitude, longitude)
-    .then(function(sunriseAndSunset) {
-      cardSunriseSunset.innerHTML = "Sunrise and Sunset: <span class=\"sunrise-sunset-text\">" + sunriseAndSunset + "</span>";
+    .then(function (sunriseAndSunset) {
+      cardSunriseSunset.innerHTML =
+        "Sunrise and Sunset: <span class=\"sunrise-sunset-text\">" + sunriseAndSunset + "</span>";
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.error(error);
-      cardSunriseSunset.innerHTML = "Sunrise and Sunset: <span class=\"sunrise-sunset-error\">" + error.message + "</span>";
+      cardSunriseSunset.innerHTML =
+        "Sunrise and Sunset: <span class=\"sunrise-sunset-error\">" + error.message + "</span>";
     });
+
+  // Toggle the .favorite class and change the star image when clicked
+  starButton.addEventListener("click", function () {
+    card.classList.toggle("favorite");
+    if (card.classList.contains("favorite")) {
+      starIcon.src = "images/star_fav.png";
+    } else {
+      starIcon.src = "images/star_unfav.png";
+    }
+  });
 
   return card;
 }
 
+// Retrieve the mountain select dropdown element
+var mountainSelect = document.getElementById("mountainSelect");
+
+// Function to populate the dropdown menu with mountain options
+function populateMountainDropdown() {
+  // Sort the mountainsArray alphabetically by name (ignoring "Mt. ")
+  mountainsArray.sort(function (a, b) {
+    var nameA = a.name.replace(/^Mt\. /i, "").toLowerCase();
+    var nameB = b.name.replace(/^Mt\. /i, "").toLowerCase();
+    return nameA.localeCompare(nameB);
+  });
+
+  // Add "All" option
+  var allOption = document.createElement("option");
+  allOption.value = "all";
+  allOption.textContent = "All";
+  mountainSelect.appendChild(allOption);
+
+  for (var mountain of mountainsArray) {
+    var option = document.createElement("option");
+    option.value = mountain.name;
+    option.textContent = mountain.name;
+    mountainSelect.appendChild(option);
+  }
+
+  // Create the "Favorites" option in the dropdown
+  var favoritesOption = document.createElement("option");
+  favoritesOption.value = "favorites";
+  favoritesOption.textContent = "Favorites";
+  mountainSelect.appendChild(favoritesOption);
+}
+
+// Function to display favorite cards
+function displayFavoriteCards() {
+  // Filter the favorite cards
+  var favoriteCards = document.querySelectorAll(".card.favorite");
+
+  // Clear existing cards
+  var mountainDetails = document.getElementById("mountainDetails");
+  mountainDetails.innerHTML = "";
+
+  // Create a row div to hold the favorite cards
+  var rowDiv = document.createElement("div");
+  rowDiv.classList.add("row");
+
+  for (var card of favoriteCards) {
+    rowDiv.appendChild(card);
+  }
+
+  mountainDetails.appendChild(rowDiv);
+}
+
+// Function to display mountain information based on the selected mountain
+function displayMountainDetails() {
+  var selectedMountain = mountainSelect.value;
+  var mountainDetails = document.getElementById("mountainDetails");
+
+  // Clear existing cards and sunrise/sunset information
+  mountainDetails.innerHTML = "";
+
+  if (selectedMountain === "all" || selectedMountain === "") {
+    // Show all mountains
+    var mountainRows = chunkArray(mountainsArray, 3);
+    for (var row of mountainRows) {
+      var rowDiv = document.createElement("div");
+      rowDiv.classList.add("row");
+
+      for (var mountain of row) {
+        var card = createMountainCard(mountain);
+        rowDiv.appendChild(card);
+      }
+
+      mountainDetails.appendChild(rowDiv);
+    }
+  } else if (selectedMountain === "favorites") {
+    // Display favorite cards
+    displayFavoriteCards();
+  } else {
+    // Find the selected mountain in the mountainsArray
+    var selectedMountainObj = mountainsArray.find(function (mountain) {
+      return mountain.name === selectedMountain;
+    });
+    if (selectedMountainObj) {
+      var card = createMountainCard(selectedMountainObj);
+      mountainDetails.appendChild(card);
+    }
+  }
+
+  // Check if any favorites exist
+  var favoriteCards = document.querySelectorAll(".card.favorite");
+  if (favoriteCards.length === 0) {
+    // No favorites, hide the "Favorites" section
+    var favoritesSection = document.getElementById("favoritesSection");
+    favoritesSection.style.display = "none";
+  } else {
+    // Favorites exist, show the "Favorites" section
+    var favoritesSection = document.getElementById("favoritesSection");
+    favoritesSection.style.display = "block";
+  }
+}
+
+// Event listener for when the page is fully loaded
+window.addEventListener("load", function () {
+  // Call the function to populate the mountain dropdown initially
+  populateMountainDropdown();
+
+  // Set the dropdown value to "all"
+  mountainSelect.value = "all";
+
+  // Display all mountains initially
+  displayMountainDetails();
+});
+
 // Event listener for when the mountain select dropdown changes
 mountainSelect.addEventListener("change", displayMountainDetails);
 
-// Call the function to populate the mountain dropdown initially
-populateMountainDropdown();
+// Function to get sunrise and sunset times for a mountain
+async function getSunriseAndSunsetForMountain(lat, lng) {
+  try {
+    var response = await fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&formatted=0`);
+    var data = await response.json();
+
+    if (response.ok) {
+      var sunriseUTC = new Date(data.results.sunrise);
+      var sunsetUTC = new Date(data.results.sunset);
+
+      var options = {
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        hour: "2-digit",
+        minute: "2-digit"
+      };
+
+      var sunriseTime = sunriseUTC.toLocaleTimeString([], options);
+      var sunsetTime = sunsetUTC.toLocaleTimeString([], options);
+
+      return `${sunriseTime} to ${sunsetTime}`;
+    } else {
+      throw new Error("Failed to retrieve sunrise and sunset data");
+    }
+  } catch (error) {
+    console.error(error);
+    return "Error retrieving sunrise and sunset data";
+  }
+}
--- Capstone2/scripts/national_parks_search.js
+++ Capstone2/scripts/national_parks_search.js
@@ -58,6 +58,46 @@ function handleFormSubmit(event) {
   displaySearchResults(filteredParks);
 }
 
+// Helper function to check if a value is "0" or 0
+function isZero(value) {
+  return value === "0" || value === 0;
+}
+
+
+// Helper function to format the address
+function formatAddress(address, city, state, zipCode) {
+  let formattedAddress = "";
+
+  if (address && address !== "0") {
+    formattedAddress += address;
+  }
+
+  if (city && city !== "0") {
+    if (formattedAddress !== "") {
+      formattedAddress += ", ";
+    }
+    formattedAddress += city;
+  }
+
+  if (state && state !== "0") {
+    if (formattedAddress !== "") {
+      formattedAddress += ", ";
+    }
+    formattedAddress += state;
+  }
+
+  if (zipCode && zipCode !== "0") {
+    if (formattedAddress !== "") {
+      formattedAddress += ", ";
+    }
+    formattedAddress += zipCode;
+  }
+
+  return formattedAddress;
+}
+
+
+
 // Function to display the search results
 function displaySearchResults(results) {
   searchResults.innerHTML = "";
@@ -65,28 +105,20 @@ function displaySearchResults(results) {
   if (results.length === 0) {
     searchResults.textContent = "No results found.";
   } else {
-    // Create row and card containers
-    var rowDiv;
-    var cardContainer;
+    let rowDiv = document.createElement("div");
+    rowDiv.className = "row";
 
-    for (var i = 0; i < results.length; i++) {
-      // Create a new row and card container for every third card
-      if (i % 3 === 0) {
-        rowDiv = document.createElement("div");
-        rowDiv.classList.add("row");
-        searchResults.appendChild(rowDiv);
-      }
-
+    for (let i = 0; i < results.length; i++) {
       // Create a Bootstrap card element
-      var card = document.createElement("div");
+      const card = document.createElement("div");
       card.className = "card mb-3 col-md-4";
 
       // Create the card header
-      var cardHeader = document.createElement("div");
+      const cardHeader = document.createElement("div");
       cardHeader.className = "card-header d-flex justify-content-between align-items-center";
 
       // Set the card title
-      var title = document.createElement("h3");
+      const title = document.createElement("h3");
       title.className = "card-title";
       title.textContent = results[i].LocationName;
       cardHeader.appendChild(title);
@@ -95,26 +127,33 @@ function displaySearchResults(results) {
       card.appendChild(cardHeader);
 
       // Create the card body
-      var cardBody = document.createElement("div");
+      const cardBody = document.createElement("div");
       cardBody.className = "card-body";
 
       // Set the address
-      var address = document.createElement("p");
+      const address = document.createElement("p");
       address.className = "card-text";
-      address.textContent = "Address: " + results[i].Address + ", " + results[i].City + ", " + results[i].State + ", " + results[i].ZipCode;
-      cardBody.appendChild(address);
+      const formattedAddress = formatAddress(results[i].Address, results[i].City, results[i].State, results[i].ZipCode);
+      if (formattedAddress !== "") {
+        address.textContent = "Address: " + formattedAddress;
+        cardBody.appendChild(address);
+      }
 
-      // Set the phone
-      var phone = document.createElement("p");
-      phone.className = "card-text";
-      phone.textContent = "Phone: " + results[i].Phone;
-      cardBody.appendChild(phone);
+      // Set the phone if it's not "0"
+      if (!isZero(results[i].Phone)) {
+        const phone = document.createElement("p");
+        phone.className = "card-text";
+        phone.textContent = "Phone: " + results[i].Phone;
+        cardBody.appendChild(phone);
+      }
 
-      // Set the fax
-      var fax = document.createElement("p");
-      fax.className = "card-text";
-      fax.textContent = "Fax: " + results[i].Fax;
-      cardBody.appendChild(fax);
+      // Set the fax if it's not "0"
+      if (results[i].Fax && !isZero(results[i].Fax)) {
+        const fax = document.createElement("p");
+        fax.className = "card-text";
+        fax.textContent = "Fax: " + results[i].Fax;
+        cardBody.appendChild(fax);
+      }
 
       // Append the card body to the card
       card.appendChild(cardBody);
@@ -122,18 +161,15 @@ function displaySearchResults(results) {
       // Append the card to the current row
       rowDiv.appendChild(card);
 
-      // Create a new row and card container for every third card
+      // Create a new row after every third card
       if ((i + 1) % 3 === 0 || (i + 1) === results.length) {
-        rowDiv = null;
-        cardContainer = null;
+        searchResults.appendChild(rowDiv);
+        rowDiv = document.createElement("div");
+        rowDiv.className = "row";
       }
     }
   }
 }
-
-
-
-
 
 // Event listener for form submission
 searchForm.addEventListener("submit", handleFormSubmit);
